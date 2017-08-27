1da287f488af28dc9d365f0c3dc85d4416290a92
Akshar Punuganti <Akshar.Punuganti@go-mmt.com>
2017-08-27 10:49:20 +0530

    add basic pom libraries

---
+++ src/main/java/com/flyppo/core/App.java
@@ -0,0 +1,13 @@
+package com.flyppo.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
